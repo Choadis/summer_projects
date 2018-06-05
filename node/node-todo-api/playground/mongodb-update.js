94fb5cbc37769ff7791e99633b2137d778414c2e
Choadis <dahlgrensm@gmail.com>
2018-06-05 02:02:07 -0400

    Fixed some errors in server.js and set up GET route

--- node/node-todo-api/playground/mongodb-update.js
+++ node/node-todo-api/playground/mongodb-update.js
@@ -1,31 +1,38 @@
 // const MongoClient = require('mongodb').MongoClient;
 const {MongoClient, ObjectID} = require('mongodb');
 
-MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, client) => {
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
   if (err) {
-    return console.log('Unable to connect to database');
+    return console.log('Unable to connect to MongoDB server');
   }
   console.log('Connected to MongoDB server');
 
-  const db = client.db('TodoApp')
+  // db.collection('Todos').findOneAndUpdate({
+  //   _id: new ObjectID('57bc4b15b3b6a3801d8c47a2')
+  // }, {
+  //   $set: {
+  //     completed: true
+  //   }
+  // }, {
+  //   returnOriginal: false
+  // }).then((result) => {
+  //   console.log(result);
+  // });
 
-  db
-  .collection('Users')
-  .findOneAndUpdate({
-    __id : new ObjectID('5b0e69911e227e173b5e3264')
+  db.collection('Users').findOneAndUpdate({
+    _id: new ObjectID('57abbcf4fd13a094e481cf2c')
   }, {
     $set: {
-      name: 'Choadis'
+      name: 'Andrew'
     },
     $inc: {
-      age: 16
+      age: 1
     }
   }, {
     returnOriginal: false
-  })
-  .then((res) => {
-    console.log(res)
+  }).then((result) => {
+    console.log(result);
   });
 
-  client.close();
+  // db.close();
 });
