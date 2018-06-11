4319a68ebea13cbe999ae14084b6abf600246d31
Choadis <dahlgrensm@gmail.com>
2018-06-11 04:01:07 -0400

    Add PATCH /todos/id in todo api

--- node/node-todo-api/server/server.js
+++ node/node-todo-api/server/server.js
@@ -1,6 +1,7 @@
-var express = require('express');
-var bodyParser = require('body-parser');
-var {ObjectID} = require('mongodb');
+const _ = require('lodash');
+const express = require('express');
+const bodyParser = require('body-parser');
+const {ObjectID} = require('mongodb');
 
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
@@ -24,8 +25,7 @@ app.post('/todos', (req, res) => {
 });
 
 app.get('/todos', (req, res) => {
-  Todo.find()
-  .then((todos) => {
+  Todo.find().then((todos) => {
     res.send({todos});
   }, (e) => {
     res.status(400).send(e);
@@ -39,14 +39,14 @@ app.get('/todos/:id', (req, res) => {
     return res.status(404).send();
   }
 
-  Todo.findById(id)
-  .then((todos) => {
-    if (!todos) {
-      return res.status(404).send()
+  Todo.findById(id).then((todo) => {
+    if (!todo) {
+      return res.status(404).send();
     }
-    res.send({todos});
+
+    res.send({todo});
   }).catch((e) => {
-    res.status(400).send()
+    res.status(400).send();
   });
 });
 
@@ -57,19 +57,45 @@ app.delete('/todos/:id', (req, res) => {
     return res.status(404).send();
   }
 
-  Todo.findByIdAndRemove(id)
-  .then((todo) => {
+  Todo.findByIdAndRemove(id).then((todo) => {
     if (!todo) {
-      return res.status(404).send()
+      return res.status(404).send();
     }
-    res.send({todo}).status(200);
+
+    res.send({todo});
   }).catch((e) => {
-    res.status(400).send()
+    res.status(400).send();
   });
 });
 
+app.patch('/todos/:id', (req, res) => {
+  var id = req.params.id;
+  var body = _.pick(req.body, ['text', 'completed']);
+
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send();
+  }
+
+  if (_.isBoolean(body.completed) && body.completed) {
+    body.completedAt = new Date().getTime();
+  } else {
+    body.completed = false;
+    body.completedAt = null;
+  }
+
+  Todo.findByIdAndUpdate(id, {$set: body}, {new: true}).then((todo) => {
+    if (!todo) {
+      return res.status(404).send();
+    }
+
+    res.send({todo});
+  }).catch((e) => {
+    res.status(400).send();
+  })
+});
+
 app.listen(port, () => {
-  console.log(`Started at port ${port}`);
+  console.log(`Started up at port ${port}`);
 });
 
 module.exports = {app};
